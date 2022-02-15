f6e4793a18ef2865f1c6aab6daf7dbd2ce14fac4
5urfacing <artom.olkov@gmail.com>
2022-02-15 20:58:01 +0600

    #3

--- solution.go
+++ solution.go
@@ -1,9 +1,9 @@
 package solution
 
 import (
-	"github.com/kyokomi/emoji/v2"
+	"github.com/kyokomi/emoji"
 )
 
 func GetMessage() string {
-	return emoji.Sprint("Hello, world :flag_gb: !!!!")
+	return emoji.Sprint("Hello :world_map:!")
 }
